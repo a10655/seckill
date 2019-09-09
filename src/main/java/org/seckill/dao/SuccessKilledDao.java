8cb2a369955f7a0da9b50b3047e7c9b8e4ad112e
win_sxd <wy_sxd@163.com>
2019-09-08 19:22:25 -0700

    fix: 添加实体bean对象，添加数据库dao文件

---
+++ src/main/java/org/seckill/dao/SuccessKilledDao.java
@@ -0,0 +1,17 @@
+package org.seckill.dao;
+
+import org.apache.ibatis.annotations.Param;
+import org.seckill.bean.SuccessKilled;
+
+public interface SuccessKilledDao {
+    /**
+     * 插入购买明细，可过滤重复（联合主键）
+     * @param seckillId
+     * @param userPhone
+     * @return
+     */
+    int insertSuccessKilled(@Param("seckillId") long seckillId, @Param("userPhone") long userPhone);
+
+    SuccessKilled queryByIdWithSeckill(@Param("seckillId") long seckillId, @Param("userPhone")long userPhone);
+
+}
